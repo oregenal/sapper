7f80aa207299d467160ccfa2bc0aa1b2bed81e8e
Andrey Gud <nosacred@gmail.com>
2021-11-27 15:48:05 +0700

    simple SDL render

--- main.c
+++ main.c
@@ -7,28 +7,47 @@
 int main(void)
 {
 	if(SDL_Init(SDL_INIT_VIDEO) != 0) {
-		SDL_Log("Unable to init SDL: %s.\n", SDL_GetError());
+		SDL_Log("Fail to initialise SDL: %s.\n", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
 
-	SDL_Surface *screen_surface = NULL;
-	SDL_Window *window = NULL;
-
-	window = SDL_CreateWindow("Sapper", SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
-			SDL_WINDOW_SHOWN);
-
-	if(window == NULL)
+	SDL_Window *window = SDL_CreateWindow("sapper", SDL_WINDOWPOS_UNDEFINED,
+											SDL_WINDOWPOS_UNDEFINED, 
+											SCREEN_WIDTH, 
+											SCREEN_HEIGHT, 
+											SDL_WINDOW_SHOWN);
+	if(window == NULL) {
+		SDL_Log("SDL_CeateWindow fail: %s.\n" , SDL_GetError());
 		exit(EXIT_FAILURE);
+	}
 
-	screen_surface = SDL_GetWindowSurface(window);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+												SDL_RENDERER_ACCELERATED | 
+												SDL_RENDERER_PRESENTVSYNC);
+	if(renderer == NULL) {
+		SDL_Log("SDL_Renderer fail: %s.\n" , SDL_GetError());
+		SDL_DestroyWindow(window);
+		exit(EXIT_FAILURE);
+	}
 
-	SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format,
-				255, 0, 0));
-	SDL_UpdateWindowSurface(window);
+	SDL_Event e;
+	int quit = 0;
+	while(!quit) {
+		while(SDL_PollEvent(&e)) {
+			switch(e.type) {
+				case SDL_QUIT:
+					quit = 1;
+					break;
+				default: {}
+			}
+		}
 
-	SDL_Delay(2000);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
 
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 
 	SDL_Quit();
